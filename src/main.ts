cd0f1e865f855d40ce666e341692a46371ddbbd1
Tom Varewyck <tvarewyck@underside.be>
2022-02-04 13:56:36 +0100

    Cors policy + Swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -7,21 +7,33 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT;
   
   // Swawgger configuration
   const config = new DocumentBuilder()
-    .setTitle('Swagger Example')
-    .setDescription('The Starter API description')
-    .setVersion('1.0')
+    .setTitle('`Em API')
+    .setDescription('The API for `Em')
+    .setVersion('1.0.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  
-  const port = process.env.PORT;
+
+  // CORS options
+  const corsOptions = {
+    "origin": true,
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "credentials": true,
+    "optionsSuccessStatus": 204,
+    "Access-Control-Allow-Headers": "*"
+  };
+
+  app.enableCors(corsOptions);
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
   await app.listen(port);
   
+  Logger.log(`Swagger documentation running on http://localhost:${port}/api`, 'Swagger');
   Logger.log(`Server started running on http://localhost:${port}`, 'Bootstrap');
 }
 
