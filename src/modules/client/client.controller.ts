d0aab8ef6342afbebe20af42d50eaad4c5ef5314
Tom Varewyck <tvarewyck@underside.be>
2022-02-08 13:57:49 +0100

    Create client with address

--- src/modules/client/client.controller.ts
+++ src/modules/client/client.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/modules/auth/guards/jwt-auth.guard';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
-@Controller('client')
-export class ClientController {}
+// DTOs
+
+
+// Services
+import { ClientService } from 'src/modules/client/client.service';
+
+@ApiTags('clients')
+@Controller('clients')
+export class ClientController {
+
+  constructor(private readonly clientService: ClientService) {
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @Post()
+  async create(@Body() client: any): Promise<any> {
+    return await this.clientService.create(client);
+  }
+
+}
--- src/modules/client/client.service.ts
+++ src/modules/client/client.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
+
+// Constants
+import { CLIENT_REPOSITORY, ADDRESS_REPOSITORY } from 'src/core/constants';
+
+// Entities
+import { Client } from 'src/core/entities/client.entity';
+import { Address } from 'src/core/entities/address.entity';
 
 @Injectable()
-export class ClientService {}
+export class ClientService {
+  constructor(@Inject(CLIENT_REPOSITORY) private clientRepository: typeof Client,
+              @Inject(ADDRESS_REPOSITORY) private addressRepository: typeof Address) {
+  }
+
+  async create(client: any): Promise<any> {
+    const addressData = client.address;
+    const clientData = client;
+    const createdAddress = await this.addressRepository.create(addressData);
+    if (!createdAddress) throw new HttpException('Cannot create client address!', HttpStatus.BAD_REQUEST);
+
+    const createdClient = await this.clientRepository.create({
+      ...clientData,
+      addressUuid: createdAddress.uuid
+    }).then(client => client.toJSON());
+    if (!createdClient) throw new HttpException('Cannot create client!', HttpStatus.BAD_REQUEST);
+
+    return {
+      ...createdClient.toJSON(),
+      address: createdAddress.toJSON()
+    }
+  }
+}
