2f34a5706045c898440531dacdbb82a736bf78df
VareTom <tomvarewyck98@gmail.com>
2022-02-12 23:58:54 +0100

    Forgot code for service output + http test request

--- src/core/dtos/service/serviceOutputDto.ts
+++ src/core/dtos/service/serviceOutputDto.ts
@@ -15,6 +15,10 @@ export class ServiceOutputDto {
   
   @ApiProperty()
   @IsOptional()
+  code?: string;
+  
+  @ApiProperty()
+  @IsOptional()
   description?: string;
   
   @ApiProperty()
@@ -29,6 +33,7 @@ export class ServiceOutputDto {
   constructor(json: any) {
     this.uuid = json.uuid;
     this.name = json.name;
+    this.code = json.code;
     this.description = json.description;
     this.priceInCent = json.priceInCent;
     this.createdAt = json.createdAt;
